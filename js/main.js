3bd05304f28eec1eae9c8fe001ed9f736848c828
Aleksey <yurchakaleksey@gmail.com>
2015-04-15 15:13:24 +0300

    More changes to index.html & styles.css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
    
-    $( "#tabs" ).tabs();
+    // $( "#tabs" ).tabs();
+    $('#mainbtn').on('click', function() {
+    	$('.left-block').show(500);
+    	$('.main-block').css('width', '60%');
 
+    } );
+
+    $('#close-left').on('click', function() {
+    	$('.left-block').hide(500);
+    	$('.main-block').css('width', '100%');
+    });
 
 });
