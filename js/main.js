53cd75dd40ec581be46a3b93812aa9f0f8e3a788
Aleksey <yurchakaleksey@gmail.com>
2015-04-16 03:29:27 +0300

    Implement AJAX query using Jquery Make HTML page & put data on it from Tabs.json

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-   
-    // $( "#tabs" ).tabs();
+   var $tbsmenu = $('#menu');
+   var $tbscontent = $('#tab-content');
     $('#mainbtn').on('click', function() {
     	$('.left-block').show(500);
     	$('.main-block').css('width', '60%');
@@ -13,4 +13,26 @@ $(document).ready(function() {
     	$('.main-block').css('width', '100%');
     });
 
+    $.ajax( {
+    	type: 'GET',
+    	url: 'data/Tabs.json',
+    	success: function(tabs) {
+    		$.each(tabs, function(index, tab) {
+    			$tbsmenu.append("<li><a>" + tab.title + "</a></li>");
+    			$tbscontent.append("<div id='id"+index+"''>"+tab.content+"</div>");
+    		});
+    	},
+    	error: function() {
+    		alert('Error loading tabs');
+    	}
+    });
+
+
+//     $.get("data/Tabs.json", function(data){
+//     	console.log(data["tabs"]);
+//     	$.each(data["tabs"], function(index, tab){
+//     		$("#menu").append("<li><a>" + tab["title"] + "</a></li>");
+//     		$(".tab-content").append("<div id='id"+index+"''>"+tab["content"]+"</div>");
+//     	});
+//     });
 });
