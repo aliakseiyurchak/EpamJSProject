2cad92cd463c1c8886e3e2f961ad590c9da01d5e
Aleksey <yurchakaleksey@gmail.com>
2015-04-17 20:38:24 +0300

    Implementation of the task

--- js/main.js
+++ js/main.js
@@ -1,33 +1,79 @@
 $(document).ready(function() {
 
-   var $tbsmenu = $('#menu');
-   var $tbscontent = $('#tab-content');
-    $('#mainbtn').on('click', function() {
-    	$('.left-block').show(500);
-    	$('.main-block').css('width', '60%');
+	var time = 500;
 
-    } );
+	$('#mainbtn').on('click', function() {
+   		$('.left-block').animate({width: "show"}, 600);
+    	$('.main-block').animate({width:"60%"}, 500);
+    });
 
     $('#close-left').on('click', function() {
-    	$('.left-block').hide(500);
-    	$('.main-block').css('width', '100%');
+   		$('.left-block').animate({width: "hide"}, 500);
+    	$('.main-block').animate({width:"100%"}, 900);
     });
+
+    var $tbsmenu = $('.menu');
+   	var $tbscontent = $('#tab-content');
+   	var $index_val = $('#index-value');
+   	var $title_val = $('#title-value');
+   	var $content_val = $('#content-value');
+
+   	function addTab (tab) {
+   		$tbsmenu.append('<li><a data-id = '+ tab.index +'>' + tab.title + '</a></li>');
+    	$tbscontent.append('<div id="id'+tab.index+'">' + tab.content + '</div>');
+   	};
 
     $.ajax( {
     	type: 'GET',
     	url: 'data/Tabs.json',
     	success: function(tabs) {
     		$.each(tabs, function(index, tab) {
-    			$tbsmenu.append("<li><a>" + tab.title + "</a></li>");
-    			$tbscontent.append("<div id='id"+index+"''>"+tab.content+"</div>");
+    			addTab(tab);
     		});
+    		console.log(tabs);
+    		 $('.menu li a').on('dblclick', function() {
+				console.log("click");
+				id = $(this).data("id");
+				$('.left-block').show(time);
+				$index_val.val(id);
+				$title_val.val($(this).html());
+				$content_val.val($("#id"+id).html());
+    			$('.main-block').css('width', '60%');
+			});
     	},
     	error: function() {
     		alert('Error loading tabs');
     	}
     });
 
+    $('#addbtn').on('click', function(){
+    	var new_tab = {
+    		title: $title_val.val(),
+    		content: $content_val.val()
+    	};
 
+    	console.log(new_tab);
+
+    	$.ajax( {
+    		type: 'POST',
+    		url: 'data/Tabs.json',
+    		data: JSON.stringify(new_tab),
+    		success: function(newTab) {
+    			addTab(newTab);
+    		},
+    		error: function() {
+    			alert('Error while saving new tab');
+    		}
+    	});
+	});
+
+	
+
+
+
+
+
+   
 //     $.get("data/Tabs.json", function(data){
 //     	console.log(data["tabs"]);
 //     	$.each(data["tabs"], function(index, tab){
@@ -36,3 +82,4 @@ $(document).ready(function() {
 //     	});
 //     });
 });
+
